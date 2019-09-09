fa8b3d14abcdddc3ccfe911e384239104dea01d1
papperslappen <papperslapp@kallerennes.com>
2019-09-09 13:17:43 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const commando = require('discord.js');
+const config = require('./config');
+
+const client = new commando.Client({
+    owner: config.discord.bot_owner,
+    commandPrefix: config.discord.prefix,
+});
+
+client
+    .on('error',console.error)
+    .on('warn',console.warn)
+    .on('debug', console.log)
+    .on('disconnect', () => { console.warn('Disconnected!'); })
+	  .on('reconnecting', () => { console.warn('Reconnecting...'); })
+    .on('ready', () => {
+        console.log(`Logged in as ${client.user.tag}!`);
+        console.log(`Connected to guilds [${client.guilds.map(v => { return v.name })}]`);
+    })
+    .on('rateLimit', (rateLimitInfo) => {
+        console.log(`WARNING! Rate limit Path: ${rateLimitInfo.path}`);
+    });
+
+require('./modules/dicebot').init(client)
+
+function main() {
+    console.log("Starting Bot");
+    if (!config.discord.login_token) {
+        console.log("login token not set");
+        process.exit(1);
+    }
+    console.log("Logging in");
+    client.login(config.discord.login_token);
+}
+
+main();
