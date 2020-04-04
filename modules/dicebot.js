701f1ae8f071741fdbc4ae74a5d606c2fc090dc4
Kalle Rennes <papperslapp@kallerennes.com>
2020-04-03 22:11:43 -0700

    fixed emojis not displaying properly

--- modules/dicebot.js
+++ modules/dicebot.js
@@ -12,18 +12,23 @@ module.exports = {
     }
 }
 
-function dice_emoji_replace(s){
-  return s.replace(/\(d4\):/g," :emoji_7: ")
-           .replace(/\(d6\):/g," :emoji_8: ")
-           .replace(/\(d8\):/g," :emoji_9: ")
-           .replace(/\(d10\):/g," :emoji_10: ")
-           .replace(/\(d20\):/g," :emoji_15: ")
-           .replace(/\(d100\):/g," :emoji_10: :emoji_10: ")
+function dice_emoji_replace(msg,s){
+  const find_client_emoji = (name) = msg.guild.emojis.find(emoji => emoji.name === name).toString();
+  if(msg.guild){
+    return s.replace(/\(d4\):/g,find_client_emoji("emoji_7"))
+             .replace(/\(d6\):/g,find_client_emoji("emoji_8"))
+             .replace(/\(d8\):/g,find_client_emoji("emoji_9"))
+             .replace(/\(d10\):/g,find_client_emoji("emoji_10"))
+             .replace(/\(d20\):/g,find_client_emoji("emoji_15"))
+             .replace(/\(d100\):/g,find_client_emoji("emoji_10") + find_client_emoji("emoji_10"))
+           } else {
+             return s
+           }
 }
 
-function format_roll_data(data){
-    if(data.number_of_rolls >= 2 && data.number_of_rolls<=15){
-      return `${dice_emoji_replace(data.formula)} = **${data.result}**`;
+function format_roll_data(msg,data){
+    if(2 <= data.number_of_rolls <= 15){
+      return `${dice_emoji_replace(client,data.formula)} = **${data.result}**`;
     }else{
       return (`🎲**${data.result}** 🎲`);
     }
@@ -36,7 +41,7 @@ async function roll(msg){
     if(response.data.result && !response.data.trivial){
       let reply = await msg.reply(`🎲${msg.content}🎲`);
       await sleep(200);
-      await reply.edit(`${format_roll_data(response.data)}`);
+      await reply.edit(`${format_roll_data(msg,response.data)}`);
     }
 }catch(error){
     if(error.response){
